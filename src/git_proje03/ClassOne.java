ec20be269e8d3dfdf8bc2322d18381737224109e
iyilmaz27 <94449626+iyilmaz27@users.noreply.github.com>
2022-03-22 15:51:53 +0300

    2. commit local masterdan

--- src/git_proje03/ClassOne.java
+++ src/git_proje03/ClassOne.java
@@ -3,5 +3,6 @@ package git_proje03;
 public class ClassOne {
     public static void main(String[] args) {
         System.out.println("local master branch");
+        System.out.println("local master branch2.commit");
     }
 }
